LineageOS/android_device_motorola_sm7435-common

sm7435-common: Use sensors HAL from hardware/motorola
Change-Id: I90a3dd06c3f949ee14e434baf859bd6d1a874729

common.mk
@@ -300,7 +300,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    sensors.moto_sm7435 \
+    sensors.moto_wrapper5 \
     android.hardware.sensors-service.multihal
 
 PRODUCT_COPY_FILES += \

4mede/android_device_motorola_sm7435-common-5.10

common.mk
@@ -302,11 +302,8 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    sensors.moto_sm7435 \
+    sensors.moto_ext \
     android.hardware.sensors-service.multihal
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \